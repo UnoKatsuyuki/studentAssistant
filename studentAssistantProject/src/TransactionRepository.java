1b31bc28007d72e887b83a64b325e8a53db709f7
zch826 <3326286521@qq.com>
2025-06-30 14:10:45 +0800

    收支记账模块

---
+++ studentAssistantProject/src/TransactionRepository.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//存储收支记录的类
+public class TransactionRepository {
+    private final List<Transaction> transactions = new ArrayList<>();  //收支记录列表
+
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+
+    public List<Transaction> getAllTransactions() {
+        return new ArrayList<>(transactions); // 返回副本，避免外部修改
+    }
+}
